25ce68894281cef65ba022a817776ee444db61b7
Vova Bilan <vovabilan89@gmail.com>
2013-03-20 16:34:28 +0200

    create color model and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,25 @@
 
 p "create roles"
 Role.create([{role: "admin"}, {role: "partner"}, {role: "viewer"}, {role: "user"}])
+
+p "create colors"
+Color.create(slug: "white")
+Color.create(slug: "black")
+Color.create(slug: "grey")
+Color.create(slug: "red")
+Color.create(slug: "orange")
+Color.create(slug: "yellow")
+Color.create(slug: "green")
+Color.create(slug: "cyan")
+Color.create(slug: "blue")
+Color.create(slug: "purple")
+Color.create(slug: "cream")
+Color.create(slug: "gold")
+Color.create(slug: "chocolate")
+Color.create(slug: "brown")
+Color.create(slug: "cornflower blue")
+Color.create(slug: "pink")
+Color.create(slug: "hot pink")
+Color.create(slug: "lime")
+Color.create(slug: "green-yellow")
+Color.create(slug: "emerald")
