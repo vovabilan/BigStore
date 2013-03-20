cf0a805ed539e2a599d526fb8a3de1f8734536b1
Vova Bilan <vovabilan89@gmail.com>
2013-03-20 12:30:39 +0200

    first commit to BigStore project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigStore::Application.config.secret_token = 'fae476d2871a318de3ced469eedf390479ebcebc1618ec93e347266b4e2186cacda611c57ffee5d442b86afb140ba26bea9ad874d1c31dd725ed58c304beae20'
